dfcf79ff8292734d0b954bb44b392bef49780bae
Robbins Cleozier <robbins.cleozier@medlypharmacy.com>
2019-12-03 14:40:57 -0500

    New test and adding build step

---
+++ src/components/DeliveryStats/tests/DeliveryStats.test.tsx
@@ -0,0 +1,44 @@
+import React from "react";
+import DeliveryStats from "../DeliveryStats";
+import renderer from "react-test-renderer";
+
+it("renders DeliveryStats correctly", () => {
+  const props = {
+    tasks: {
+        total: { 
+            completedOntime: 2,
+            completedLate: 3,
+            completed: 5,
+            failedOntime: 2,
+            failedLate: 2,
+            failed: 4,
+            scheduled: 9
+        }
+    }
+  }
+  const component = renderer
+    .create(<DeliveryStats  {...props} />)
+    .toJSON();
+  expect(component).toMatchSnapshot();
+});
+
+it("renders DeliveryStats correctly with new props", () => {
+    const props = {
+      tasks: {
+          total: { 
+              completedOntime: 12,
+              completedLate: 13,
+              completed: 15,
+              failedOntime: 12,
+              failedLate: 12,
+              failed: 14,
+              scheduled: 29
+          }
+      }
+    }
+    const component = renderer
+      .create(<DeliveryStats  {...props} />)
+      .toJSON();
+    expect(component).toMatchSnapshot();
+  });
+  
