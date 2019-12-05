5552abac6738287be7eea72d49142380874b7ae8
Robbins Cleozier <robbins.cleozier@medlypharmacy.com>
2019-12-05 14:42:57 -0500

    Updating deploy task and nav

--- src/layout/Navigation/Navigation.tsx
+++ src/layout/Navigation/Navigation.tsx
@@ -7,9 +7,9 @@ const Navigation = () => {
   return (
     <div className="row p-2 mt-n1">
       <div className="navigation col-12 p-2 mt-n4">
-        <Link to="/">Realtime Delivery</Link>
+        <Link to="/">Realtime Delivery Data</Link>
         <span className="navigation-spacer"> &bull;  </span>
-        <Link to="/historical">Historical Delivery</Link>
+        <Link to="/historical">Historical Delivery Data</Link>
       </div>
     </div>
   );
